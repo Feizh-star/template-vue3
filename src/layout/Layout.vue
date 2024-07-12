59c884b0ca3374fa85d258b03e938a702ee87f4e
tujiahao <tujiahao@cnpresky.com>
2024-07-12 14:14:28 +0800

    优化layout中的menuList

--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -58,7 +58,7 @@ export default defineComponent({
 import { RouterView } from 'vue-router'
 import { useMenu } from '@/store/menu'
 const menu = useMenu()
-const menuList = menu.getMenuList
+const menuList = computed(() => menu.getMenuList)
 
 // Layout和RouterSocket组件添加props判断路由路径匹配，避免vue-router的bug：KeepAlive出现不同分支时，失活的分支的子组件会被重复渲染
 const props = defineProps<{
