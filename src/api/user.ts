428fa929f300337e3b708f9bc863e3647d635a44
tujiahao <tujiahao@cnpresky.com>
2025-03-04 17:27:38 +0800

    屏蔽登录相关接口

--- src/api/user.ts
+++ src/api/user.ts
@@ -2,36 +2,60 @@ import request from '@/utils/request'
 
 // 获取验证码
 export function getValidImage() {
-  return request({
-    url: '/captchaImage',
-    method: 'get',
+  return new Promise((resolve) => {
+    resolve({
+      code: 200,
+      img: '',
+      uuid: '123'
+    })
   })
+  // return request({
+  //   url: '/captchaImage',
+  //   method: 'get',
+  // })
 }
 
 // 登录方法
 export function login(data: ILoginParams) {
-  return request({
-    url: '/login',
-    headers: {
-      isToken: false,
-    },
-    method: 'post',
-    data: data,
+  return new Promise((resolve) => {
+    resolve({
+      code: 200,
+      token: '1125648964163',
+    })
   })
+  // return request({
+  //   url: '/login',
+  //   headers: {
+  //     isToken: false,
+  //   },
+  //   method: 'post',
+  //   data: data,
+  // })
 }
 
 // 退出方法
 export function logout() {
-  return request({
-    url: '/logout',
-    method: 'post',
+  return new Promise((resolve) => {
+    resolve('')
   })
+  // return request({
+  //   url: '/logout',
+  //   method: 'post',
+  // })
 }
 
 // 获取用户详细信息
 export function getInfo() {
-  return request({
-    url: '/getInfo',
-    method: 'get',
+  return new Promise((resolve) => {
+    resolve({
+      code: 200,
+      roles: '',
+      user: '123',
+      permissions: ''
+    })
   })
+  // return request({
+  //   url: '/getInfo',
+  //   method: 'get',
+  // })
 }
